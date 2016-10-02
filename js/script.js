39a00e13d4067bc79945088c9b162179a99f2c72
kodandaram199 <kodandaram199@gmail.com>
2016-10-02 10:13:14 +0530

    improved filter using check boxes

--- js/script.js
+++ js/script.js
@@ -34,16 +34,27 @@ app.filter('check', function() {
     var filtered = [];
     
     angular.forEach(items, function(item) {
-       if(compileStatus.Accepted == false ) {
+      if(compileStatus.Accepted == false && compileStatus.Error == false &&   compileStatus.Wrong == false && compileStatus.Time==false) {
           filtered.push(item);
         }
-        else if(compileStatus.Accepted == true && item.compiler_status == 'Accepted'){
+        else if(compileStatus.Accepted == true && compileStatus.Error == false && item.compiler_status == 'Accepted' &&  compileStatus.Wrong==false && compileStatus.Time==false){
           filtered.push(item);
         }
+        else if(compileStatus.Error == true && compileStatus.Accepted == false && item.compiler_status == 'Compilation error' &&  compileStatus.Wrong==false && compileStatus.Time==false){
+          filtered.push(item);
+        }
+		else if(compileStatus.Error == false && compileStatus.Accepted == false && compileStatus.Wrong==true && item.compiler_status.toLowerCase().indexOf("wrong")!=-1 && compileStatus.Time==false){
+          filtered.push(item);
+        }
+		
+		else if(compileStatus.Error == false && compileStatus.Accepted == false && compileStatus.Time==true && compileStatus.Wrong==false && item.compiler_status.toLowerCase().indexOf("limit")!=-1){
+          filtered.push(item);
+        }
+		
     });
   
     return filtered;
-  };
+  };		
 });
 
 
@@ -54,9 +65,9 @@ app.filter('check', function() {
 app.controller('AppController', function($scope, $filter,$http)
             {       
               
-              $scope.compileStatus = {Accepted: false};
+              $scope.compileStatus = {Accepted: false, Error: false, Wrong:false, Time:false };
               
-             $http.get('http://hackerearth.0x10.info/api/ctz_coders?type=json&query=list_submissions&page=1').success(function(data, status, headers, config) {
+             $http.get('http://hackerearth.0x10.info/api/ctz_coders?type=json&query=list_submissions&page=1347').success(function(data, status, headers, config) {
 		$scope.items = data.websites;
 	}).error(function(data, status, headers, config) {
 		console.log("No data found..");
